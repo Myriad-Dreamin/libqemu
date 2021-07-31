27f10a02c28cbb98b59cf00f6a0b9c55ab1cff52
Myriad-Dreamin <xyangxi5@gmail.com>
2021-07-31 12:39:42 +0800

    dev: add libqemu mmio api decls

--- include/kfuzz/qemu.h
+++ include/kfuzz/qemu.h
@@ -5,10 +5,18 @@
 #ifndef LIBQEMU_QEMU_H
 #define LIBQEMU_QEMU_H
 
+typedef uint64_t qemu_hw_addr_t;
+
 int kfuzz_qemu_module_begin(int argc, char **argv, char **envp);
+
 int kfuzz_qemu_module_init_bios(void);
+
 int kfuzz_qemu_module_loop(void);
+
 int kfuzz_qemu_module_cleanup(void);
 
+int kfuzz_qemu_mmio_read(qemu_hw_addr_t phys_addr, uint64_t *data, uint32_t len);
+
+int kfuzz_qemu_mmio_write(qemu_hw_addr_t phys_addr, uint64_t data, uint32_t len);
 
 #endif //LIBQEMU_QEMU_H
